394773de5e3c22bfd70a9f28d2cf7ae1b49b1375
Duy Tran <duyvutran2303@gmail.com>
2024-05-23 18:08:15 -0400

    day2 + day3: refactor test

--- challenges/day3/day3_test.go
+++ challenges/day3/day3_test.go
@@ -6,8 +6,7 @@ import (
 	"github.com/rael346/aoc2023-golang/internal/utils"
 )
 
-func TestPart1Given(t *testing.T) {
-	result := Part1(utils.ToInput(`467..114..
+var givenInput = `467..114..
 ...*......
 ..35..633.
 ......#...
@@ -16,26 +15,20 @@ func TestPart1Given(t *testing.T) {
 ..592.....
 ......755.
 ...$.*....
-.664.598..`))
+.664.598..`
+
+func TestPart1Given(t *testing.T) {
+	result := Part1(utils.ToInput(givenInput))
 	utils.ExpectInt(t, result, 4361)
 }
 
 func TestPart1Actual(t *testing.T) {
 	result := Part1(utils.ReadInputFromFile("input.txt"))
-	utils.ExpectInt(t, result, -1)
+	utils.ExpectInt(t, result, 557705)
 }
 
 func TestPart2Given(t *testing.T) {
-	result := Part2(utils.ToInput(`467..114..
-...*......
-..35..633.
-......#...
-617*......
-.....+.58.
-..592.....
-......755.
-...$.*....
-.664.598..`))
+	result := Part2(utils.ToInput(givenInput))
 	utils.ExpectInt(t, result, 467835)
 }
 
