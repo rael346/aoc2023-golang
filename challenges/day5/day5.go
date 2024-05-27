58e8ebb46a8b5ba8ccf40955bde7a8628e93bfe9
Duy Tran <duyvutran2303@gmail.com>
2024-05-26 22:47:12 -0400

    day5

---
+++ challenges/day5/day5.go
@@ -0,0 +1,136 @@
+package day5
+
+import (
+	"math"
+	"strconv"
+	"strings"
+)
+
+func Part1(input []string) int {
+	seedStrs := strings.Fields(input[0])[1:]
+	srcMap := map[int]int{}
+	for _, seedStr := range seedStrs {
+		seed, _ := strconv.Atoi(seedStr)
+		srcMap[seed] = seed
+	}
+
+	for _, line := range input[2:] {
+		if strings.Contains(line, "map") {
+			continue
+		}
+
+		if line == "" {
+			newSrcMap := map[int]int{}
+			for _, dest := range srcMap {
+				newSrcMap[dest] = dest
+			}
+			srcMap = newSrcMap
+			continue
+		}
+
+		mapping := strings.Fields(line)
+		destStart, _ := strconv.Atoi(mapping[0])
+		srcStart, _ := strconv.Atoi(mapping[1])
+		rangeLen, _ := strconv.Atoi(mapping[2])
+
+		for src := range srcMap {
+			if srcStart <= src && src < srcStart+rangeLen {
+				srcMap[src] = src - srcStart + destStart
+			}
+		}
+	}
+
+	minLoc := math.MaxInt
+
+	for _, dest := range srcMap {
+		minLoc = min(minLoc, dest)
+	}
+
+	return minLoc
+}
+
+func Part2(input []string) int {
+	seedStrs := strings.Fields(input[0])[1:]
+	srcs := make([][2]int, len(seedStrs)/2)
+	for i := 0; i < len(seedStrs); i += 2 {
+		seed, _ := strconv.Atoi(seedStrs[i])
+		rangeLen, _ := strconv.Atoi(seedStrs[i+1])
+		srcs[i/2] = [2]int{seed, seed + rangeLen - 1}
+	}
+
+	unmappedSrcs := [][2]int{}
+	dest := [][2]int{}
+	for _, line := range input[2:] {
+		if strings.Contains(line, "map") {
+			continue
+		}
+
+		if line == "" {
+			srcs = append(srcs, dest...)
+			dest = [][2]int{}
+			continue
+		}
+
+		mapping := strings.Fields(line)
+		destStart, _ := strconv.Atoi(mapping[0])
+		srcMapStart, _ := strconv.Atoi(mapping[1])
+		srcMapRange, _ := strconv.Atoi(mapping[2])
+		srcMapEnd := srcMapStart + srcMapRange - 1
+
+		for _, src := range srcs {
+			newSrc, unmapped := Intersect(src, [2]int{srcMapStart, srcMapEnd}, destStart)
+			if newSrc[0] != -1 && newSrc[1] != -1 {
+				dest = append(dest, newSrc)
+			}
+			unmappedSrcs = append(unmappedSrcs, unmapped...)
+		}
+		srcs = unmappedSrcs
+		unmappedSrcs = [][2]int{}
+	}
+
+	minLoc := math.MaxInt
+	for _, srcRange := range append(srcs, dest...) {
+		minLoc = min(minLoc, srcRange[0])
+	}
+
+	return minLoc
+}
+
+func Intersect(src [2]int, srcMap [2]int, destStart int) ([2]int, [][2]int) {
+	srcStart, srcEnd := src[0], src[1]
+	srcMapStart, srcMapEnd := srcMap[0], srcMap[1]
+	unmapped := make([][2]int, 0, 2)
+
+	if srcStart > srcMapEnd || srcMapStart > srcEnd {
+		unmapped = append(unmapped, src)
+		return [2]int{-1, -1}, unmapped
+	}
+
+	if srcMapStart <= srcStart && srcEnd <= srcMapEnd {
+		newSrcStart := srcStart - srcMapStart + destStart
+		newSrcEnd := srcEnd - srcMapStart + destStart
+		return [2]int{newSrcStart, newSrcEnd}, unmapped
+	}
+
+	if srcMapStart <= srcStart && srcEnd > srcMapEnd {
+		newSrcStart := srcStart - srcMapStart + destStart
+		newSrcEnd := srcMapEnd - srcMapStart + destStart
+		unmapped = append(unmapped, [2]int{srcMapEnd + 1, srcEnd})
+		return [2]int{newSrcStart, newSrcEnd}, unmapped
+	}
+
+	if srcMapStart > srcStart && srcEnd <= srcMapEnd {
+		newSrcEnd := srcEnd - srcMapStart + destStart
+		unmapped = append(unmapped, [2]int{srcStart, srcMapStart - 1})
+		return [2]int{destStart, newSrcEnd}, unmapped
+	}
+
+	if srcMapStart > srcStart && srcEnd > srcMapEnd {
+		newSrcEnd := srcMapEnd - srcMapStart + destStart
+		unmapped = append(unmapped, [2]int{srcStart, srcMapStart - 1})
+		unmapped = append(unmapped, [2]int{srcMapEnd + 1, srcEnd})
+		return [2]int{destStart, newSrcEnd}, unmapped
+	}
+
+	panic("unreachable case")
+}
---
+++ challenges/day5/day5_test.go
@@ -0,0 +1,61 @@
+package day5
+
+import (
+	"testing"
+
+	"github.com/rael346/aoc2023-golang/internal/utils"
+)
+
+var givenInput = `seeds: 79 14 55 13
+
+seed-to-soil map:
+50 98 2
+52 50 48
+
+soil-to-fertilizer map:
+0 15 37
+37 52 2
+39 0 15
+
+fertilizer-to-water map:
+49 53 8
+0 11 42
+42 0 7
+57 7 4
+
+water-to-light map:
+88 18 7
+18 25 70
+
+light-to-temperature map:
+45 77 23
+81 45 19
+68 64 13
+
+temperature-to-humidity map:
+0 69 1
+1 0 69
+
+humidity-to-location map:
+60 56 37
+56 93 4`
+
+func TestPart1Given(t *testing.T) {
+	result := Part1(utils.ToInput(givenInput))
+	utils.ExpectInt(t, result, 35)
+}
+
+func TestPart1Actual(t *testing.T) {
+	result := Part1(utils.ReadInputFromFile("input.txt"))
+	utils.ExpectInt(t, result, 57075758)
+}
+
+func TestPart2Given(t *testing.T) {
+	result := Part2(utils.ToInput(givenInput))
+	utils.ExpectInt(t, result, 46)
+}
+
+func TestPart2Actual(t *testing.T) {
+	result := Part2(utils.ReadInputFromFile("input.txt"))
+	utils.ExpectInt(t, result, 31161857)
+}
